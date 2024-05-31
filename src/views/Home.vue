d5f72248502522743faf624e91747f2a51ebf3b7
codeAmbivert <titilopechisom20@gmail.com>
2024-05-31 07:57:20 +0100

    chore

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,97 +1,96 @@
 <template>
-  <!-- <div class="min-h-[100vh] w-full bg-[#010409]">
+  <div v-if="loading" class="loading-state">Loading...</div>
+  <div v-else class="min-h-[100vh] w-full bg-[#010409] p-5" v-cloak>
+    <!-- <ToastContainer /> -->
+
     <div class="flex flex-col items-start gap-5">
-        <router-link to="/" class="text:lg sm:text-xl text-blue-400">
-          &lt;ALT/SOE/023/2353.&gt;
-        </router-link>
-      </div>
-      <div class="min-h-[100vh] max-w-6xl mx-auto pt-20 w-full">
-        <div class="w-full flex flex-col items-center">
-          {user ? (
-            <div class="w-full flex gap-5 mt-10">
-              <div class="hidden sm:block w-1/4">
-                <div class="w-full h-auto rounded-full border-2 border-[#31343A] overflow-hidden">
+      <router-link to="/" class="text:lg sm:text-xl text-blue-400">
+        &lt;ALT/SOE/023/2353.&gt;
+      </router-link>
+    </div>
+    <div class="min-h-[100vh] max-w-6xl mx-auto pt-20 w-full">
+      <div class="w-full flex flex-col items-center">
+        <div v-if="user" class="w-full flex gap-5 mt-10">
+          <div class="hidden sm:block w-1/4">
+            <div class="w-full h-auto rounded-full border-2 border-[#31343A] overflow-hidden">
+              <img
+                v-if="user.data.avatar_url"
+                :src="user.data.avatar_url || githubLogo"
+                alt="user"
+                class="w-full h-auto"
+              />
+            </div>
+          </div>
+          <div class="w-full border-2 border-[#30363D] rounded-xl p-5 text-white">
+            <div class="flex justify-between items-center flex-wrap gap-5">
+              <div class="text-start flex gap-5">
+                <div
+                  class="h-16 w-16 block sm:hidden rounded-full border-2 border-[#31343A] overflow-hidden"
+                >
                   <img
-                    src={user?.data?.avatar_url || githubLogo}
+                    v-if="user.data.avatar_url"
+                    :src="user.data.avatar_url || githubLogo"
                     alt="user"
                     class="w-full h-auto"
                   />
                 </div>
+                <div>
+                  <h1 v-if="user.data.name" class="font-semibold text-3xl">
+                    {{ user.data.name }}
+                  </h1>
+                  <p v-if="user.data.login" class="text-lg font-medium text-[#31343A]">
+                    {{ user.data.login }}
+                  </p>
+                </div>
               </div>
-              <div class="w-full border-2 border-[#30363D] rounded-xl p-5 text-white">
-                <div class="flex justify-between items-center flex-wrap gap-5">
-                  <div class="text-start flex gap-5">
-                    <div class="h-16 w-16 block sm:hidden rounded-full border-2 border-[#31343A] overflow-hidden">
-                      <img
-                        src={user?.data?.avatar_url || githubLogo}
-                        alt="user"
-                        class="w-full h-auto"
-                      />
-                    </div>
-                    <div>
-                      <h1 class="font-semibold text-3xl">
-                        {user?.data?.name}
-                      </h1>
-                      <p class="text-lg font-medium text-[#31343A]">
-                        {user?.data?.login}
-                      </p>
-                    </div>
-                  </div>
-                  <div class="flex gap-5">
-                    <div>Followers: {user?.data?.followers}</div>
-                    <div>Following: {user?.data?.following}</div>
-                  </div>
-                </div>
-                <div class="text-start mt-5 flex flex-col gap-2 text-[#656E76]">
-                  <Link
-                    to="repositories"
-                    class="text-blue-400 font-medium"
-                  >
-                    Goto Repositories
-                  </Link>
-                  <p>
-                    <span class="font-medium">Email:</span>{" "}
-                    {user?.data?.email}
-                  </p>
-                  {user?.data?.bio && (
-                    <p>
-                      <span class="font-medium">Bio:</span>{" "}
-                      {user?.data?.bio}
-                    </p>
-                  )}
-                  {user?.data?.location && (
-                    <p>
-                      <span class="font-medium">Location:</span>{" "}
-                      {user?.data?.location}
-                    </p>
-                  )}
-
-                  <p>
-                    <span class="font-medium">Public repositories:</span>{" "}
-                    {user?.data?.public_repos}
-                  </p>
-                  {user?.data?.total_private_repos && (
-                    <p>
-                      <span class="font-medium">Private repositories:</span>{" "}
-                      {user?.data?.total_private_repos}
-                    </p>
-                  )}
-
-                  <router-link
-                    to="/error-boundary"
-                    class="text-blue-400 font-medium"
-                  >
-                    Error boundary page
-                  </router-link>
-                </div>
+              <div class="flex gap-5">
+                <div v-if="user.data.followers">Followers: {{ user.data.followers }}</div>
+                <div v-if="user.data.following">Following: {{ user.data.following }}</div>
               </div>
             </div>
-  </div> -->
-  <div>Home</div>
+            <div class="text-start mt-5 flex flex-col gap-2 text-[#656E76]">
+              <router-link to="/repositories" class="text-blue-400 font-medium">
+                Goto Repositories
+              </router-link>
+              <p>
+                <span class="font-medium" v-if="user.data.email">Email:</span>
+                {{ user.data.email }}
+              </p>
+
+              <p v-if="user.data.bio">
+                <span class="font-medium">Bio:</span>
+                {{ user.data.bio }}
+              </p>
+
+              <p v-if="user.data.location">
+                <span class="font-medium" v-if="user.data.location">Location:</span>
+                {{ user.data.location }}
+              </p>
+
+              <p>
+                <span class="font-medium" v-if="user.data.public_repos">Public repositories:</span>
+                {{ user.data.public_repos }}
+              </p>
+
+              <p v-if="user.data.total_private_repos">
+                <span class="font-medium">Private repositories:</span>
+                {{ user.data.total_private_repos }}
+              </p>
+
+              <router-link to="/error-boundary" class="text-blue-400 font-medium">
+                Error boundary page
+              </router-link>
+            </div>
+          </div>
+        </div>
+
+        <img v-else :src="githubLogo" alt="github" class="max-w-md w-full h-auto mt-20" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue'
 import githubLogo from '@/assets/githubLogo.svg'
 import axios from 'axios'
 
@@ -101,7 +100,8 @@ export default {
     return {
       user: null,
       githubLogo: githubLogo,
-      token: import.meta.env.VITE_GITHUB_TOKEN
+      token: import.meta.env.VITE_GITHUB_TOKEN,
+      loading: true
     }
   },
   methods: {
@@ -115,13 +115,17 @@ export default {
         this.user = response
       } catch (error) {
         console.log(error)
+      } finally {
+        this.loading = false
       }
-      console.log(this.user)
     }
   },
-  mounted() {
-    this.getUserDetails()
+  async mounted() {
+    try {
+      await this.getUserDetails()
+    } catch (error) {
+      console.log(error)
+    }
   }
-  // Your component options here
 }
 </script>
